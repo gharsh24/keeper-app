1bc27d9231c4d73138b9778a76de23116a619bbd
Harsh Goyal <harsh.sg360@gmail.com>
2023-07-09 10:27:20 +0530

    Upload the files

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import CreateIcon from '@mui/icons-material/Create';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateIcon />
+        Note It.
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
